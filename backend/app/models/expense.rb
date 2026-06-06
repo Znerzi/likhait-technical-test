fe3a088ed118221d3039c985c12b3c3fdc974fd0
Znerzi <diazrenz18@gmail.com>
2026-06-06 18:20:51 +0800

    prevent future date in expense

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -1,3 +1,16 @@
 class Expense < ApplicationRecord
   belongs_to :category
+
+  # prevent user from adding future date
+  validate :date_cannot_be_in_future
+
+  private
+
+  # check if selected date is beyond today
+  def date_cannot_be_in_future
+    if date.present? && date > Date.today
+      errors.add(:date, "cannot be in the future")
+    end
+  end
+
 end
